71ae475a0419987f0b068d372fb5eeadde34540f
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2022-05-19 14:51:20 -0300

    :tada: 19-05

--- 7. Redes Neurais Artificiais em Python/ajuste_pesos.py
+++ 7. Redes Neurais Artificiais em Python/ajuste_pesos.py
@@ -11,15 +11,24 @@ import pandas as pd
 
 lista_x = [0, 0, 1, 1]
 lista_y = [0, 1, 0, 1]
-resultados = []
 
+resultados_e = []
+resultados_ou = []
+esperados_e = []
+esperados_ou = []
+
+# operador E
 for i in range(len(lista_x)):
-    resultado = operador_E(lista_x[i], lista_y[i])
-    resultados.append(resultado)
+    esperado_e = lista_x[i] and lista_y[i]
+    esperados_e.append(esperado_e)
+    
+    esperado_ou = lista_x[i] or lista_y[i]
+    esperados_ou.append(esperado_ou)
     
 OP_E = pd.DataFrame({
     'x1':lista_x, 
     'x2':lista_y, 
-    'classe':resultados})
+    'classe_E':esperados_e,
+    'classe_OU':esperados_ou})
 
 print(OP_E)

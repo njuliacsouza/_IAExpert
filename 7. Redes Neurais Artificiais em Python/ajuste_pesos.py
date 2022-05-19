d36fa6cd3dd81f2dff2c492e3f6547d84ac11486
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2022-05-19 14:57:06 -0300

    :tada: 19-05

--- 7. Redes Neurais Artificiais em Python/ajuste_pesos.py
+++ 7. Redes Neurais Artificiais em Python/ajuste_pesos.py
@@ -16,16 +16,17 @@ lista_y = [0, 1, 0, 1]
 esperados_e = [(lista_x[i] and lista_y[i]) for i in range(len(lista_x))]
 esperados_ou = [(lista_x[i] or lista_y[i]) for i in range(len(lista_x))]
 
-    
+# tabela verdade
 OP_E = pd.DataFrame({
     'x1':lista_x, 
     'x2':lista_y, 
     'classe_E':esperados_e,
     'classe_OU':esperados_ou})
 
+print(OP_E)
+
 # ajuste dos pesos
 resultados_e = []
 resultados_ou = []
 
-print(OP_E)
 #print(pesos)

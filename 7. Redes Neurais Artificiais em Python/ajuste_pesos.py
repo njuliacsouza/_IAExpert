ef7d4953f869aa489b7016b531db886c11a1a479
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2022-05-19 15:45:46 -0300

    :tada: 19-05

--- 7. Redes Neurais Artificiais em Python/ajuste_pesos.py
+++ 7. Redes Neurais Artificiais em Python/ajuste_pesos.py
@@ -30,23 +30,28 @@ print(OP_E)
 peso_inicial = [0, 0]
 pesos_e = np.array(peso_inicial)
 pesos_ou = np.array(peso_inicial)
-erros_e = []
-erros_ou = []
-
-for i in range(len(lista_x)):
-    resultado_e = (lista_x[i] * pesos_e[0]) + (lista_y[i] * pesos_e[1]) 
-    resultado_ou = (lista_x[i] * pesos_ou[0]) + (lista_y[i] * pesos_ou[1]) 
+erros_e = [10 for _ in range(len(lista_x))]
+erros_ou = [10 for _ in range(len(lista_x))]
+erros = erros_e + erros_ou
+i = 0
     
-    erro_e = esperados_e[i] - resultado_e
-    erro_ou = esperados_ou[i] - resultado_ou
-    erros_e.append(erro_e)
-    erros_ou.append(erro_ou)
-    
-    if (erro_e > 0.001):
-        pesos_e = pesos_e + (0.1 * lista_x[i]*erro_e)
-    elif (erro_ou > 0.001):
-        pesos_ou = pesos_ou + (0.1 * lista_x[i]*erro_e)
+while max(erros)>0.01:
+    i += 1
+    for i in range(len(lista_x)):
+        resultado_e = (lista_x[i] * pesos_e[0]) + (lista_y[i] * pesos_e[1]) 
+        resultado_ou = (lista_x[i] * pesos_ou[0]) + (lista_y[i] * pesos_ou[1]) 
         
-
+        erro_e = esperados_e[i] - resultado_e
+        erro_ou = esperados_ou[i] - resultado_ou
+        erros_e[i] = erro_e
+        erros_ou[i] = erro_ou
+        
+        if (erro_e > 0.001):
+            pesos_e = pesos_e + (0.1 * lista_x[i]*erro_e)
+        elif (erro_ou > 0.001):
+            pesos_ou = pesos_ou + (0.1 * lista_x[i]*erro_ou)
+    erros = erros_e + erros_ou
+        
 print('Pesos operador E:',pesos_e)
 print('Pesos operador OU:',pesos_ou)
+print('Iterações:',i)

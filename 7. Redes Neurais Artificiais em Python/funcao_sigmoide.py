10ee202d6996012674ba4df2e2b076aef428663e
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2022-05-31 18:00:08 -0300

    :tada: estudos 31-05

---
+++ 7. Redes Neurais Artificiais em Python/funcao_sigmoide.py
@@ -0,0 +1,14 @@
+"""
+IAExpert Academy - Redes Neurais Artificiais em Python
+
+May, 2022
+
+Maria Júlia Cristofoletti de Souza
+"""
+
+#import pandas as pd
+from NeuralNetwork import sigmoid_function
+
+a = sigmoid_function(50)
+b = sigmoid_function(0)
+c = sigmoid_function(-50)
--- 7. Redes Neurais Artificiais em Python/rede_multicamada.py
+++ 7. Redes Neurais Artificiais em Python/rede_multicamada.py
@@ -6,9 +6,28 @@ May, 2022
 Maria Júlia Cristofoletti de Souza
 """
 
-#import pandas as pd
+import pandas as pd
 from NeuralNetwork import sigmoid_function
 
-a = sigmoid_function(50)
-b = sigmoid_function(0)
-c = sigmoid_function(-50)
+
+lista_x = [0, 0, 1, 1]
+lista_y = [0, 1, 0, 1]
+entradas_xy = [[lista_x[i],lista_y[i]] for i in range(len(lista_x))]
+
+# operador XOR esperado
+esperados_xor = [0,1,1,0]
+
+# tabela verdade
+OP = pd.DataFrame({
+    'x1':lista_x, 
+    'x2':lista_y, 
+    'classe_XOR':esperados_xor
+    })
+
+print(OP)
+
+# ajuste dos pesos  
+#pesos_xor, i_xor = ajuste_pesosI(entradas_xy, esperados_short, 0.1)
+    
+#print('Pesos operador XOR:',[round(i, 4) for i in pesos_short])
+#print('Iterações XOR:',i_short)

5695ca4cb25af4bede3294813209d61af62bab03
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2022-05-31 14:20:26 -0300

    :tada: estudos 31-05

--- 7. Redes Neurais Artificiais em Python/ajuste_pesos.py
+++ 7. Redes Neurais Artificiais em Python/ajuste_pesos.py
@@ -16,6 +16,7 @@ entradas_xy = [[lista_x[i],lista_y[i]] for i in range(len(lista_x))]
 # operador E, OU, esperado
 esperados_e = [(lista_x[i] & lista_y[i]) for i in range(len(lista_x))]
 esperados_ou = [(lista_x[i] | lista_y[i]) for i in range(len(lista_x))]
+#esperados_short = [0,1,1,0]
 
 # tabela verdade
 OP_E = pd.DataFrame({
@@ -27,10 +28,15 @@ OP_E = pd.DataFrame({
 print(OP_E)
 
 # ajuste dos pesos  
-pesos_e, i_e = ajuste_pesos(entradas_xy, esperados_e)
-pesos_ou, i_ou = ajuste_pesos(entradas_xy, esperados_ou)
+pesos_e, i_e = ajuste_pesos(entradas_xy, esperados_e,)
+pesos_ou, i_ou = ajuste_pesos(entradas_xy, esperados_ou,)
+#pesos_short, i_short = ajuste_pesos(entradas_xy, esperados_short, 0.1)
     
 print('Pesos operador E:',[round(i, 4) for i in pesos_e])
 print('Iterações E:',i_e)
 print('Pesos operador OU:',[round(i, 4) for i in pesos_ou])
 print('Iterações OU:',i_ou)
+#print('Pesos operador SHORT:',[round(i, 4) for i in pesos_short])
+#print('Iterações OU:',i_short)
+
+# operador SHORT não é linearmente seprável (erro não chega a ZERO)
